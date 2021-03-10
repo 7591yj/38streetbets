e6c758c50bb3298101af31e89c9a4b5b6a4c4004
Yeongjin Kim <7591blitzloft@gmail.com>
2021-03-11 00:05:21 +0900

    Initial commit
    Link finder added, sends embed

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 import discord
 from discord.ext import commands
+from scrapper import get_request, find_name_link
 
 app = commands.Bot(command_prefix='#38')
 
 @app.event
 async def on_ready():
-    print('Logging in: ')
-    print(app.user.name)
-    print('connection was succesful')
+    print('Logging in: '+app.user.name)
+    print('Connection was succesful')
     await app.change_presence(status=discord.Status.online, activity=discord.Game(name='떡상기원'))
+
+@app.command(name = "찾기")
+async def posts(ctx, name):
+    encoded = get_request(name)
+    stock = find_name_link(encoded)
+    embed = discord.Embed(title = '결과', color=discord.Color.dark_blue())
+    for stocks in stock:
+        embed.add_field(name = stocks['name'], value = "[%s](%s)"%('링크', stocks['link']), inline=False)
+    await ctx.send(embed=embed)
 
 app.run("")
--- scrapper.py
+++ scrapper.py
@@ -5,28 +5,33 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWe
 
 url = "https://finance.naver.com/search/searchList.nhn?query="
 
-def get_request()
+def get_request(name):
     # Search input & incode to euc-kr
-    search_for = input("Search for: ")
-    encode_euc_kr = search_for.encode('euc-kr')
+    encode_euc_kr = name.encode('euc-kr')
     to_string = str(encode_euc_kr)
     # to_string has escape codes, cleaning up
     replace_text = to_string.replace(r"\x", "%").strip("b").split("'")
     search_url = url+replace_text[1]
+    return search_url
 
-def find_name_link(search_url)
-    # get the page and find names of the stock
-    response = requests.get(search_url, headers=headers)
-    response.encoding = 'euc-kr'
-    soup = BeautifulSoup(response.text, "html.parser")
-    find_table = soup.find("table", {"class": "tbl_search"})
-    href_list = find_table.find_all("td", {"class": "tit"})
-    # dict for saving stock name and web address
-    stock = {}
-    # find name and web addresses 
-    # for each stock and save them to stock dict
-    for items in href_list:
-    name = items.find("a").text
-    find_href = items.find("a")["href"].strip(" ")
-    href = "https://finance.naver.com"+find_href
-    stock[name]=href
+def save_stock(stock):
+  name = stock.find("a").text
+  find_href = stock.find("a")["href"].strip(" ")
+  href = "https://finance.naver.com"+find_href
+  return {
+    'name': name,
+    'link' : href
+  }
+
+def find_name_link(search_url):
+  stocks = []
+  # get the page and find names of the stock
+  response = requests.get(search_url, headers=headers)
+  response.encoding = 'euc-kr'
+  soup = BeautifulSoup(response.text, "html.parser")
+  find_table = soup.find("table", {"class": "tbl_search"})
+  href_list = find_table.find_all("td", {"class": "tit"})
+  for item in href_list:
+    stock = save_stock(item)
+    stocks.append(stock)
+  return stocks
